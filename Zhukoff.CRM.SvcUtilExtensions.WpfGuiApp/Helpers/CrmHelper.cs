4374ffc1eb6863ffac28ce408b1251d43448579b
Grigory Zhukov <grigory@zhukoff.pro>
2016-09-14 15:26:23 +0300

    Added WPF GUI project

---
+++ Zhukoff.CRM.SvcUtilExtensions.WpfGuiApp/Helpers/CrmHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zhukoff.CRM.SvcUtilExtensions.WpfGuiApp.Helpers
+{
+    public class CrmHelper
+    {
+    }
+}
